131c5f6c6ee57a2c4c6bab374a8d93ebe30b77bf
sandermi <sander.middelkoop@dhl.com>
2019-10-25 10:23:21 +0200

    Created the basic functionality as required in the exercise with the fewest lines of code, added a new endpoint which accepts two pathvariables and 3 parameters. Sums two distances.

--- src/main/java/com/example/demo/SumDistancesController.java
+++ src/main/java/com/example/demo/SumDistancesController.java
@@ -1,10 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -29,5 +26,22 @@ public class SumDistancesController {
     public String convertYardsToMeters(@PathVariable double yards){
         yards = metersToYardsConverter.convertFrom(yards);
         return yards + " meters";
+    }
+
+    @GetMapping("/distance/{distance1}/{distance2}")
+    public double sumDistances(@PathVariable double distance1,
+                               @PathVariable double distance2,
+                               @RequestParam(value = "isYardsUnit1", required = false, defaultValue = "false") boolean isYardsUnit1,
+                               @RequestParam(value = "isYardsUnit2", required = false, defaultValue = "false") boolean isYardsUnit2,
+                               @RequestParam(value = "isYardsResult", required = false, defaultValue = "false") boolean isYardsResult) {
+
+        //Convert both values to meters for simplicity: otherwise I would be using complicated switch statements
+        //or if-blocks
+        double distance1Meters = isYardsUnit1 ? metersToYardsConverter.convertFrom(distance1) : distance1;
+        double distance2Meters = isYardsUnit2 ? metersToYardsConverter.convertFrom(distance2) : distance2;
+
+        //If the result should be in yards, sum the values and convert to yards. Else just sum the values.
+        return isYardsResult ? metersToYardsConverter.convertTo
+                (distance1Meters + distance2Meters) : distance1Meters + distance2Meters;
     }
 }
