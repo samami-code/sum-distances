6a089d278cee9e28211dd8ef64a8449f9596992f
sandermi <sander.middelkoop@dhl.com>
2019-10-25 13:03:56 +0200

    Added some basic info to the info message at the root

--- src/main/java/com/example/demo/Messages.java
+++ src/main/java/com/example/demo/Messages.java
@@ -2,5 +2,10 @@ package com.example.demo;
 
 public class Messages {
     public static final String RESULT = "%.2f %s + %.2f %s = %.2f %s!";
-    public static final String INFO = "Testing, testing, 1 2 3";
+    public static final String INFO = "This is the root. Using this webservice, one can sum two distances " +
+            "by navigating to /distance/{distance1}/{distance2}. Additionally you can specify whether each distance is " +
+            "in yards or meters by setting the parameter(s) isYardsUnit1 and/or isYardsUnit2 to true/false. " +
+            "The default value is false. Finally, you can specify whether the result should be in yards or meters " +
+            "by setting the parameter isYardsResult to true/false. The default is false. " +
+            "Example usage: /distance/5/5?isYardsUnit1=true&isYardsUnit2=true&isYardsResult=true";
 }
