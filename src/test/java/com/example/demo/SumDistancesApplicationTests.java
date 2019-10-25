4124e3d19d537e48b5b848e49775f8b2c75d6249
sandermi <sander.middelkoop@dhl.com>
2019-10-25 13:11:53 +0200

    Added some basic tests that execute when running with command clean test spring-boot:run

--- src/test/java/com/example/demo/SumDistancesApplicationTests.java
+++ src/test/java/com/example/demo/SumDistancesApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 class SumDistancesApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void testLandingPage() throws Exception {
+		this.mockMvc.perform(get("/"))
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	void testDistanceSummationWhenNoUnitsSpecified() throws Exception {
+		this.mockMvc.perform(get("/distance/5/5"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("5,00 meters + 5,00 meters = 10,00 meters!"));
+	}
+
+	@Test
+	void testDistanceSummationResultIsYards() throws Exception {
+		this.mockMvc.perform(get("/distance/5/5?isYardsResult=true"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("5,00 meters + 5,00 meters = 10,94 yards!"));
+	}
+
+	@Test
+	void testDistanceSummationEverythingIsYards() throws Exception  {
+		this.mockMvc.perform(get("/distance/5/5?isYardsUnit1=true&isYardsUnit2=true&isYardsResult=true"))
+				.andExpect(status().isOk())
+				.andExpect(content().string("5,00 yards + 5,00 yards = 10,00 yards!"));
 	}
 
 }
